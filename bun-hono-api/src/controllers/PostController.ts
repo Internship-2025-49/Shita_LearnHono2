7df413c860f1bb651ef4a0c8728b44895849fdc8
Shita Zeny <shita0639@gmail.com>
2025-02-06 09:19:29 +0700

    feat: add API endpoint for delete posts and create new function deletePost in controller #

--- bun-hono-api/src/controllers/PostController.ts
+++ bun-hono-api/src/controllers/PostController.ts
@@ -121,3 +121,25 @@ export async function updatePost(c: Context) {
       console.error(`Error updating post: ${e}`);
   }
 }
+
+export async function deletePost(c: Context) {
+  try {
+
+      // Konversi tipe id menjadi number
+      const postId = parseInt(c.req.param('id'));
+
+      //delete post with prisma
+      await prisma.post.delete({
+          where: { id: postId },
+      });
+
+      //return JSON
+      return c.json({
+          success: true,
+          message: 'Post Deleted Successfully!',
+      }, 200);
+
+  } catch (e: unknown) {
+      console.error(`Error deleting post: ${e}`);
+  }
+}
--- bun-hono-api/src/routes/index.ts
+++ bun-hono-api/src/routes/index.ts
@@ -2,7 +2,7 @@
 import { Hono } from 'hono';
 
 //import controller
-import { getPosts, createPost, getPostById, updatePost } from '../controllers/PostController';
+import { getPosts, createPost, getPostById, updatePost, deletePost } from '../controllers/PostController';
 
 //inistialize router
 const router = new Hono()
@@ -12,5 +12,6 @@ router.get('/', (c) => getPosts(c));
 router.post('/', (c) => createPost(c));
 router.get('/:id', (c) => getPostById(c));
 router.patch('/:id', (c) => updatePost(c));
+router.delete('/:id', (c) => deletePost(c));
 
 export const Routes = router;
